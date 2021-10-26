4c91f208372586606013824625b3a0ab66804980
jeffabarbieri <jeffreyabarbieri@gmail.com>
2021-10-26 14:20:19 -0400

    Added Footer class and updated App.js

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import MyOutfits from './MyOutfits'
 import MyCloset from './MyCloset'
+import Footer from './Footer'
 
 function App() {
   return (
     <div className="App">
-      <MyCloset/>
+      <MyOutfits/>
+      <Footer/>
     </div>
   );
 }
