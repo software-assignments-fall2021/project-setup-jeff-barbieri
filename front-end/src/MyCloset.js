c7cf8dcc6d81ad02dd6efeeadf9a1f263f0e7b64
Brian Rios <bjr366@nyu.edu>
2021-10-27 13:47:42 -0400

    try on button, underlined clothing headings,

--- front-end/src/MyCloset.js
+++ front-end/src/MyCloset.js
@@ -6,7 +6,7 @@ const Clothing = (props) => {
 		<section className="clothing">
 			<h2 className="clothingHead">{props.heading}</h2>
 			<img className="clothingImg" alt={props.alt} src={props.src}/>
-			<p>{props.details}</p>
+            <button className="tryOn" type="button">Try On</button>
 		</section>
 	)
 }
@@ -67,43 +67,61 @@ const MyCloset = () => {
                 </div>
             </div>
 
+            {/* TODO, in the future this search bar will search through a Clothing database */}
+            {/* search bar */}
+            <div class="search-container">
+                <form role="search" id="form">
+                    <input type="search" id="query" name="q"
+                        placeholder="Search..."
+                        aria-label="Search through clothing types">
+                    </input>
+                    <button>
+                        <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
+                    </button>
+                </form>
+                {/* <form>
+                    <input type="text" placeholder="Search.." name="search"></input>
+                    <button type="submit"><i></i></button>
+                </form> */}
+            </div>
+
             {/* TODO in the future, we will have to extract these Clothing 
             objects from an API */}
 
             {/* tops section */}
             <section className="tops">
                 {/* hideable content */}
-				<Clothing heading="Top One Name" alt="Clothing One" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Top Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Top Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Top Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
+				<Clothing heading="Top One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
+				<Clothing heading="Top Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
+				<Clothing heading="Top Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
+				<Clothing heading="Top Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
 			</section>
 
             {/* bottoms section */}
             <section className="bottoms">
                 {/* hideable content */}
-				<Clothing heading="Bottom One Name" alt="Clothing One" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Bottom Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Bottom Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Bottom Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
+				<Clothing heading="Bottom One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
+				<Clothing heading="Bottom Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
+				<Clothing heading="Bottom Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
+				<Clothing heading="Bottom Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
             </section>
 
             {/* accessories section */}
             <section className="accessories">
                 {/* hideable content */}
-				<Clothing heading="Accessories One Name" alt="Clothing One" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Accessories Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Accessories Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Accessories Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
+				<Clothing heading="Accessories One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
+				<Clothing heading="Accessories Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
+				<Clothing heading="Accessories Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
+				<Clothing heading="Accessories Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
 			</section>
 
             {/* footwear section */}
             <section className="footwear">
                 {/* hideable content */}
-				<Clothing heading="Footwear One Name" alt="Clothing One" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Footwear Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Footwear Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Clothing heading="Footwear Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
+				<Clothing heading="Footwear One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
+				<Clothing heading="Footwear Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
+				<Clothing heading="Footwear Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
+				<Clothing heading="Footwear Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
 			</section>
 
 
