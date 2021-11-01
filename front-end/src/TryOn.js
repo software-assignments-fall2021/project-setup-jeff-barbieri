b2dabb93f6b98e6a12ca92980c40810c9d054a75
Frank Weng <wengf2086@gmail.com>
2021-11-01 01:20:06 -0400

    modified TryOn.js

--- front-end/src/TryOn.js
+++ front-end/src/TryOn.js
@@ -17,7 +17,7 @@ const TryOn = () => {
         console.log("Congrats! This Outfit has been added to My Outfits.")
     }
 
-    // function that hides and shows clothing tab content. Test.
+    // function that hides and shows clothing tab content.
     function tabHide(){
         let tabNames = new Array("tops", "bottoms", "accessories", "footwear");
         let currentTab; 
@@ -45,35 +45,13 @@ const TryOn = () => {
 		<div className="tryOn">
 			<div className="heading">Try On</div>
 
-            {/* TODO mannequin*/}
-            <img id="mannequin" src="https://picsum.photos/220/220" alt="Mannequin"></img>
-
-            {/* TODO Add to Outfits button */}
-            <button id="outfitButton" type="button">Add to Outfits</button>
-			
-            {/* tab dropdown menu */}
-            <div className="menu">
-                <div className="tab-item" id="tops-tab" onClick={tabHide}>
-                    Tops
-                </div>
-                <div className="tab-item" id="bottoms-tab" onClick={tabHide}>
-                    Bottoms
-                </div>
-                <div className="tab-item" id="accessories-tab" onClick={tabHide}>
-                    Accessories
-                </div>
-                <div className="tab-item" id="footwear-tab" onClick={tabHide}>
-                    Footwear
-                </div>
-            </div>
-
             {/* TODO, in the future this search bar will search through a Clothing database */}
             {/* search bar */}
             <div class="search-container">
                 <form role="search" id="form">
                     <input type="search" id="query" name="q"
                         placeholder="Search..."
-                        aria-label="Search through clothing types">
+                        aria-label="Paste URL Here">
                     </input>
                     <button>
                         <svg viewBox="0 0 1024 1024"><path class="path1" d="M848.471 928l-263.059-263.059c-48.941 36.706-110.118 55.059-177.412 55.059-171.294 0-312-140.706-312-312s140.706-312 312-312c171.294 0 312 140.706 312 312 0 67.294-24.471 128.471-55.059 177.412l263.059 263.059-79.529 79.529zM189.623 408.078c0 121.364 97.091 218.455 218.455 218.455s218.455-97.091 218.455-218.455c0-121.364-103.159-218.455-218.455-218.455-121.364 0-218.455 97.091-218.455 218.455z"></path></svg>
@@ -84,47 +62,28 @@ const TryOn = () => {
                     <button type="submit"><i></i></button>
                 </form> */}
             </div>
+            
+            {/* TODO mannequin*/}
+            <img id="mannequin" src="https://picsum.photos/220/220" alt="Mannequin"></img>
+
+            {/* TODO Add to Closet button */}
+            <button id="outfitButton" type="button">Add to Closet</button>
+			{/* TODO Add to Remove button */}
+            <button id="outfitButton" type="button">Remove</button>
+            {/* TODO Add to Add to Outfits button */}
+            <button id="outfitButton" type="button">Add to Outfits</button>
 
             {/* TODO in the future, we will have to extract these Clothing 
             objects from an API */}
 
-            {/* tops section */}
-            <section className="tops">
+            {/* recent try-ons section */}
+            <section className="recents">
                 {/* hideable content */}
-				<Clothing heading="Top One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
-				<Clothing heading="Top Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
-				<Clothing heading="Top Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
-				<Clothing heading="Top Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
+				<Clothing heading="Clothing One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
+				<Clothing heading="Clothing Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
+				<Clothing heading="Clothing Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
+				<Clothing heading="Clothing Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
 			</section>
-
-            {/* bottoms section */}
-            <section className="bottoms">
-                {/* hideable content */}
-				<Clothing heading="Bottom One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
-				<Clothing heading="Bottom Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
-				<Clothing heading="Bottom Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
-				<Clothing heading="Bottom Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
-            </section>
-
-            {/* accessories section */}
-            <section className="accessories">
-                {/* hideable content */}
-				<Clothing heading="Accessories One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
-				<Clothing heading="Accessories Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
-				<Clothing heading="Accessories Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
-				<Clothing heading="Accessories Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
-			</section>
-
-            {/* footwear section */}
-            <section className="footwear">
-                {/* hideable content */}
-				<Clothing heading="Footwear One Name" alt="Clothing One" src="https://picsum.photos/220/220" />
-				<Clothing heading="Footwear Two Name" alt="Clothing Two" src="https://picsum.photos/220/220" />
-				<Clothing heading="Footwear Three Name" alt="Clothing Three" src="https://picsum.photos/220/220" />
-				<Clothing heading="Footwear Four Name" alt="Clothing Four" src="https://picsum.photos/220/220" />
-			</section>
-
-
 		</div>
 	)
 }
