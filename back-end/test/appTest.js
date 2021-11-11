42dfe032a5c4fa39a84620714e710d16c4b5a1dc
jeffabarbieri <jeffreyabarbieri@gmail.com>
2021-11-11 16:14:02 -0500

    Starting unit testing in appTest.js

--- back-end/test/appTest.js
+++ back-end/test/appTest.js
@@ -1,9 +1,25 @@
+const chai = require('chai');
+const mocha = require('mocha');
 const assert = require('assert');
 
-describe('Array', function() {
+describe('GET MyCloset', () => {
+  it('it should GET MyCloset', (done) => {
+    chai.request(server)
+        .get('/mycloset.json')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('JSON');
+              res.body.length.should.be.eql(0);
+          done();
+        });
+  });
+});
+
+/*describe('JSON', function() {
     describe('#indexOf()', function() {
-      it('should return -1 when the value is not present', function() {
-        assert.equal([1, 2, 3].indexOf(4), -1);
+      it('should return -1 when the object is empty', function() {
+        assert.equal(app.get(tryon).indexOf(0), -1);
       });
     });
-  });
+  });*/
+
