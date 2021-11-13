7aea64ea123427f6db89d418537bfaf3e3d32f3d
jeffabarbieri <jeffreyabarbieri@gmail.com>
2021-11-13 15:53:41 -0500

    Added schema for mannequin, login, tryon

--- database/schema/schema.js
+++ database/schema/schema.js
@@ -1,11 +1,40 @@
-//filler schema for database implementation
-const closet = new Schema(
-    {
-      name: String,
-      type: String,
+//schema for closet values
+const closet = new Schema({
+      name: String, //name of clothing article
+      type: String, //clothing type, maybe replace with int later?
       size: {type: Number,
         min: 0,
-        max: 4}, //store clothing sizes as integer values
-      source: String,
-      alt: String
+        max: 4}, //store clothing sizes as integer values, we can decide how many later
+      source: String, //image URL
+      alt: String //alt text for image
     })
+
+//schema for mannequin values
+const mannequin = new Schema({
+        sex: Boolean, //I believe our visualizer only supports M/F, but this can be updated
+        height: Number,
+        shirt: Number,
+        pants: Number,
+        jacket: Number,
+        waist: Number,
+        weight: Number,
+        body: Number
+    })
+
+//schema for user authentication values
+const login = new Schema({
+        username: String,
+        password: String //this will have to be encrypted in our .env file
+    })
+
+//schema for try on values
+const tryOn = new Schema({
+    name: String, //name of clothing article
+    type: String, //clothing type, maybe replace with int later?
+    size: {type: Number,
+        min: 0,
+        max: 4}, //store clothing sizes as integer values, we can decide how many later
+    source: String, //image URL
+    alt: String, //alt text for image
+    inCloset: Boolean //store if article is in closet
+})
