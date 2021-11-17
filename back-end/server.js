9791c3c63c114e915485a0571047d1c9988b9585
Brian Rios <bjr366@nyu.edu>
2021-11-17 14:25:03 -0500

    connecting react to express

--- back-end/server.js
+++ back-end/server.js
@@ -2,14 +2,23 @@
 const express = require('express')
 const app = express()
 const server = require("./app") // load up the web server
-const port = 3000 // the port to listen to for incoming requests
+const port = process.env.PORT || 5000 // the port to listen to for incoming requests
+
+// OLD WAY
 // call express's listen function to start listening to the port
-const listener = server.listen(port, function () {
-  console.log(`Server running on port: ${port}`)
-})
+// const listener = server.listen(port, function () {
+//   console.log(`Server running on port: ${port}`)
+// })
+
+// app.get('/back-end', (req, res) => {
+//   res.send({express: 'EXPRESS BACK END CONNECTED TO REACT'})
+// })
+
+// NEW WAY (Brian)
+app.listen(port, () => console.log(`Listening on port ${port}`))
 
 app.get('/back-end', (req, res) => {
-  res.send({express: 'EXPRESS BACK END CONNECTED TO REACT'})
+  res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' }); //Line 10
 })
 
 // a function to stop listening to the port
