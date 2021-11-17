fb51426d9a96d1ccfc320afd2098de9172b69123
Brian Rios <bjr366@nyu.edu>
2021-11-17 14:49:35 -0500

    front end update

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,12 +2,17 @@ import React from 'react'
 import MyOutfits from './MyOutfits'
 import MyCloset from './MyCloset'
 import TryOn from './TryOn'
+import MyMannequin from './MyMannequin'
+import Footer from './Footer'
 import Authentication from './Authentication';
 import Login from './Login';
 import NavMenu from './components/NavMenu';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom'; 
+import { Component } from 'react'
 
 function App() {
+  //TODO connect react to express
+  
   return (
     <div className="App">
       
@@ -24,19 +29,22 @@ function App() {
           </Route>
 
           {/* TODO: MyMannequin */}
+          <Route path='/MyMannequin'>
+            <MyMannequin/>
+          </Route>
 
           <Route path='/TryOn'>
             <TryOn/>
           </Route>
+          
+          {/* TODO fix footer to every page */}
+          <Footer/>
           
         </Switch>
 
       </Router>
       {/* <Authentication/>
       <Login/> */}
-
-      {/* <MyCloset/> */}
-      {/* <MyOutfits/> */}
     </div>
   );
 }
