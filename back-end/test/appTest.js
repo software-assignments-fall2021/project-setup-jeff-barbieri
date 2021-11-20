7b42ad628156967b87beb0df4f9ce460d26ef82f
jeffabarbieri <jeffreyabarbieri@gmail.com>
2021-11-20 14:06:02 -0500

    Added tests for back-end functions

--- back-end/test/appTest.js
+++ back-end/test/appTest.js
@@ -15,6 +15,45 @@ describe('GET MyCloset', () => {
   });
 });
 
+describe('GET MyOutfits', () => {
+  it('it should GET MyOutfits.json', (done) => {
+    chai.request(server)
+        .get('/myoutfits.json')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('JSON');
+              res.body.length.should.be.eql(0);
+          done();
+        });
+  });
+});
+
+describe('GET TryOn', () => {
+  it('it should GET TryOn.json', (done) => {
+    chai.request(server)
+        .get('/tryon.json')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('JSON');
+              res.body.length.should.be.eql(0);
+          done();
+        });
+  });
+});
+
+describe('GET MyMannequin', () => {
+  it('it should GET MyMannequin.json', (done) => {
+    chai.request(server)
+        .get('/mymannequin.json')
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('JSON');
+              res.body.length.should.be.eql(0);
+          done();
+        });
+  });
+});
+
 /*describe('JSON', function() {
     describe('#indexOf()', function() {
       it('should return -1 when the object is empty', function() {
@@ -22,4 +61,3 @@ describe('GET MyCloset', () => {
       });
     });
   });*/
-
