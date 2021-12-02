96a17ced8d2dcd7bc9beb45b5ace616eade997a0
Brian Rios <bjr366@nyu.edu>
2021-12-02 16:29:40 -0500

    display MyOutfits data

--- front-end/src/MyOutfits.js
+++ front-end/src/MyOutfits.js
@@ -12,14 +12,45 @@ const Outfit = (props) => {
 }
  
 const MyOutfits = () => {
+	function DisplayOutfits() {
+		console.log("starting outfits function")
+		const [data, setData] = React.useState([]);
+
+		React.useEffect(() => {
+			fetch("/my-outfits")
+				.then((res) => res.json())
+				.then((data) => setData(data.outfits))
+				.catch(console.error);
+		}, []);
+
+		console.log(data)
+		if (data) {
+			console.log("data exists");
+			return (
+				<React.Fragment>
+					{data.map((outfit) => (
+						<Outfit
+							heading={outfit.heading} 
+							alt = {outfit.alt}
+							src={outfit.src}
+							details={outfit.details}
+						/>
+
+					))}
+				</React.Fragment>
+			);
+		}
+		return null;
+	}
 	return(
 		<div className="myOutfits">
 			<div className="heading">My Outfits</div>
 			<section>
-				<Outfit heading="Outfit One Name" alt="Outfit One" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
+				{DisplayOutfits()}
+				{/* <Outfit heading="Outfit One Name" alt="Outfit One" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
 				<Outfit heading="Outfit Two Name" alt="Outfit Two" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
 				<Outfit heading="Outfit Three Name" alt="Outfit Three" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
-				<Outfit heading="Outfit Four Name" alt="Outfit Four" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/>
+				<Outfit heading="Outfit Four Name" alt="Outfit Four" src="https://picsum.photos/220/220" details="Shirt — Lorem, Pants — Ipsum, Shoes — Dolor, Jacket — Sit Amet, Belt — Consectetur."/> */}
 			</section>
 		</div>
 	)
