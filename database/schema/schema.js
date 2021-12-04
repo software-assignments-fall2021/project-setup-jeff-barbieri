14dddf2374f208badf03c756dc25746cba1ab817
jeffabarbieri <jeffreyabarbieri@gmail.com>
2021-12-04 15:26:37 -0500

    Updated schema before adding to MongoDB

--- database/schema/schema.js
+++ database/schema/schema.js
@@ -1,5 +1,5 @@
 //schema for clothing values
-const clothing = new Schema({
+const Clothing = new Schema({
     name: String, //name of clothing article
     type: String, //clothing type, maybe replace with int later?
     size: String, //decided not to enumerate size
@@ -7,8 +7,57 @@ const clothing = new Schema({
     alt: String //alt text for image
 })
 
+//schema for each category of clothing
+
+//schema for clothing tops
+const Tops = new Schema({
+    name: String,
+    type: String,
+    size: String,
+    color: String,
+    length: Number,
+    width: Number,
+    sleeve: Number,
+    source: String
+})
+
+//schema for clothing bottoms
+const Bottoms = new Schema({
+    name: String,
+    type: String,
+    size: String,
+    color: String,
+    waist: Number,
+    rise: Number,
+    inseam: Number,
+    source: String
+})
+
+//schema for footwear
+const Footwear = new Schema({
+    name: String,
+    type: String,
+    size: Number,
+    color: String,
+    insole: Number,
+    width: Number,
+    source: String
+})
+
+//schema for accessories
+//NOTE: this is the most arbitrary category, so this may be revised
+const Accessories = new Schema({
+    name: String,
+    type: String,
+    size: String,
+    color: String,
+    length: Number,
+    width: Number,
+    source: String
+})
+
 //schema for closet values
-const closet = new Schema({
+const Closet = new Schema({
     //all entries should be arrays of each clothing element type
     tops: [],
     bottoms: [],
@@ -17,7 +66,7 @@ const closet = new Schema({
 })
 
 //schema for mannequin values
-const mannequin = new Schema({
+const Mannequin = new Schema({
     sex: Boolean, //I believe our visualizer only supports M/F, but this can be updated
     height: Number,
     shirt: Number,
@@ -29,14 +78,14 @@ const mannequin = new Schema({
 })
 
 //schema for user authentication values
-const login = new Schema({
+const Login = new Schema({
     username: String,
     // email: String, not sure if our login includes email address
     password: String //this will have to be encrypted in our .env file
 })
 
 //schema for try on values
-const tryOn = new Schema({
+const TryOn = new Schema({
     name: String, //name of clothing article
     type: String, //clothing type, maybe replace with int later?
     size: {type: Number,
@@ -48,7 +97,7 @@ const tryOn = new Schema({
 })
 
 //schema for outfit values
-const outfits = new Schema({
+const Outfits = new Schema({
     //outfits will be array of clothing items (array of arrays)
     outfits: [[]]
 })
