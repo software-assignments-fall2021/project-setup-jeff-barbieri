2ca34582ef2ddfd0fe4a13579bcaf3efc055f1bd
Brian Rios <bjr366@nyu.edu>
2021-12-05 18:36:40 -0500

    semicolons

--- back-end/app.js
+++ back-end/app.js
@@ -1,20 +1,20 @@
 // import and instantiate express
-const express = require("express") // CommonJS import style!
-const app = express() // instantiate an Express object
-const path = require("path")
+const express = require("express"); // CommonJS import style!
+const app = express(); // instantiate an Express object
+const path = require("path");
 
 // import some useful middleware
-const multer = require("multer") // middleware to handle HTTP POST requests with file uploads
-const axios = require("axios") // middleware for making requests to APIs
-require("dotenv").config({ silent: true }) // load environmental variables from a hidden file named .env
-const morgan = require("morgan") // middleware for nice logging of incoming HTTP requests
+const multer = require("multer"); // middleware to handle HTTP POST requests with file uploads
+const axios = require("axios"); // middleware for making requests to APIs
+require("dotenv").config({ silent: true }); // load environmental variables from a hidden file named .env
+const morgan = require("morgan"); // middleware for nice logging of incoming HTTP requests
 
 // use the morgan middleware to log all incoming http requests
-app.use(morgan("dev")) // morgan has a few logging default styles - dev is a nice concise color-coded style
+app.use(morgan("dev")); // morgan has a few logging default styles - dev is a nice concise color-coded style
 
 // use express's builtin body-parser middleware to parse any data included in a request
-app.use(express.json()) // decode JSON-formatted incoming POST data
-app.use(express.urlencoded({ extended: true })) // decode url-encoded incoming POST data
+app.use(express.json()); // decode JSON-formatted incoming POST data
+app.use(express.urlencoded({ extended: true })); // decode url-encoded incoming POST data
 
 // import fs to read json files
 'use strict';
@@ -25,8 +25,7 @@ const mongoose = require('mongoose');
 
 // TODO Brian: Connect Database to back-end
 
-// display environment variables
-console.log(process.env);
+// environment variables for mongodb
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 
