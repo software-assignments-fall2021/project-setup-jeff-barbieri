152cac6c73dd344f366776ebac6ac2b9a6f57adc
Olivia Huang <oh486@nyu.edu>
2021-12-06 23:21:44 -0500

    connected register

--- back-end/server.js
+++ back-end/server.js
@@ -2,12 +2,17 @@
 const express = require('express')
 const app = express()
 const server = require("./app") // load up the web server
-const port = process.env.PORT || 5000 // the port to listen to for incoming requests
+// const port = process.env.PORT || 5000 // the port to listen to for incoming requests
+// import routes
+const authRoutes = require("./routes/auth");
 
-// listen for port
-const listener = server.listen(port, function () {
-  console.log(`Server running on port: ${port}`)
-})
+
+//route middlewares
+app.use("/api/user", authRoutes)
+
+// Listen on port 5000
+app.listen(5000, () => console.log("server is running..."));
+
 
 // a function to stop listening to the port
 const close = () => {
